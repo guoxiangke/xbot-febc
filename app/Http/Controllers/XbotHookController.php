c8af80b41eb8e4d3484b02aaa6ada53df3c6f73a
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-02-14 07:27:04 -0800

    add callback

---
+++ app/Http/Controllers/XbotHookController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+// use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use App\Services\Xbot;
+
+
+
+class XbotHookController extends Controller
+{
+    public function __invoke(Request $request){
+        // 验证消息
+        if(!isset($request['msgid']) || $request['self'] == true)  return $this->_return();
+
+        $wxidOrCurrentRoom = $request['wxid'];
+        $isRoom = Str::endsWith($wxidOrCurrentRoom, '@chatroom');
+        // personal
+        $this->wxid = $wxidOrCurrentRoom;
+        $this->remark = Str::replace("\n", '', $request['remark']);
+        $this->remark = Str::replace(":", '', $request['remark']);
+        if($isRoom){
+             $this->wxid = $request['from'];
+             $this->remark = $request['from_remark'];
+        }
+        // $this->cache = Cache::tags($this->wxid);
+		$adminGroupWxid = config('febc.group.forward.from'); //测试群
+		$groupsToForward = config('febc.group.forward.to');
+		$msgid = $request['msgid'];
+		//TODO  && msg_type = Link!
+    	if($isRoom && $wxidOrCurrentRoom == $adminGroupWxid){
+			$msg = [
+				'type' => 'forward',
+				'to' => 'filehelper',
+				'data' => compact('msgid'),
+			];
+			collect($groupsToForward)->each(function($wxid) use($msg){
+				$msg['to'] = $wxid;
+				$this->send($msg);
+			});
+    	}
+
+        $keyword = $request['content'];
+        return $this->_return();
+  
+    }
+
+    private function _return(){
+        return response()->json(null);
+    }
+
+    private function send($msg){
+	    // $msg = [
+	    //     'type' => 'text',
+	    //     'to' => $wxid,
+	    //     'data' => [
+	    //         'content' => $content
+	    //     ],
+	    // ];
+
+	    // $msg = [
+	    //     'type' => 'forward',
+	    //     'to' => $wxid,
+	    //     'data' => [
+	    //         'msgid' => $msgid
+	    //     ],
+	    // ];
+	    return Http::withToken(config('services.xbot.token'))
+	            ->post(config('services.xbot.endpoint'), $msg);
+    }
+
+}
---
+++ config/febc.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'group' => [
+		//群转发配置
+		'forward'=>[
+			'from' => env('GROUP_FWD_FROM', '?@chatroom'),
+			'to' => array_map('trim', explode(',', env('GROUP_FWD_TO'))),
+		],
+	],
+];
