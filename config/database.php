587f723a7000e2f2991d07ccab9b46d91ee22a0a
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-02-14 19:53:56 -0800

    add tls and timeout of redis

--- config/database.php
+++ config/database.php
@@ -129,21 +129,27 @@ return [
         ],
 
         'default' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'read_timeout' => 600,
+            'timeout' => 600,
         ],
 
         'cache' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'read_timeout' => 600,
+            'timeout' => 600,
         ],
 
     ],
